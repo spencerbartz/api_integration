27a1db16283a9864c7624a453a95b3ffe8cf88be
spencerbartz <spencerbartz@gmail.com>
2019-10-16 17:25:14 -0700

    Tidy up indentation, add comments

--- product_list.js
+++ product_list.js
@@ -1,68 +1,70 @@
 
-// API URL 
-var url = '';
+// API Endpoint URL with API token
+var url = 'https://www.apiexample.com?apiKey=example-api-key';
 
 // Send GET request to API
 fetch(url, {
-    method: "GET",
-    mode: "cors"
-  })
-  .catch(() => {
-    console.error("Could not reach server");
-  })
-  .then((res) => {
-    var table = document.getElementById("productList");
-    res.json().then((json) => {
-        // JSON must be fetched by the response as a promise
-        console.log(json["products"]);
+  method: "GET",
+  mode: "cors"
+})
+.catch(() => {
+  console.error("Could not reach server");
+})
+.then((res) => {
+  var table = document.getElementById("productList");
 
-        // Loop through products and attach desired data to DOM
-        json["products"].forEach((product) => {
+  // JSON must be fetched by the response as a promise
+  res.json().then((json) => {
 
-          // Create new row for this product
-          var tr = document.createElement("tr");
+    // Loop through products and attach desired data to DOM
+    json["products"].forEach((product) => {
 
-          // Add product name to row
-          var nameTd = document.createElement("td");
-          var nameNode = document.createTextNode(product["name"]);
-          nameTd.appendChild(nameNode);
-          tr.appendChild(nameTd);
+      // Create new row for this product
+      var tr = document.createElement("tr");
 
-          // Add thumbnailImage to row if available. Use image, message as fallbacks
-          var img = document.createElement("img");
-          var imgTd = document.createElement("td");
+      // Add product name to row
+      var nameTd = document.createElement("td");
+      var nameNode = document.createTextNode(product["name"]);
+      nameTd.appendChild(nameNode);
+      tr.appendChild(nameTd);
 
-          if (product["thumbnailImage"]) {
-            img.src = product["thumbnailImage"];
-            imgTd.appendChild(img);
-          } else if (product["image"]) {
-            img.src = product["thumbnailImage"];
-            imgTd.appendChild(img);
-          } else {
-            var node = document.createTextNode("No Image Available");
-            imgTd.appendChild(node);
-          }
+      // Add thumbnailImage to row if available. Use image, message as fallbacks
+      var img = document.createElement("img");
+      var imgTd = document.createElement("td");
 
-          tr.appendChild(imgTd);
+      if (product["thumbnailImage"]) {
+        img.src = product["thumbnailImage"];
+        imgTd.appendChild(img);
+      } else if (product["image"]) {
+        img.src = product["thumbnailImage"];
+        imgTd.appendChild(img);
+      } else {
+        var node = document.createTextNode("No Image Available");
+        imgTd.appendChild(node);
+      }
 
-          // Add regularPrice to row
-          if (product["regularPrice"]) {
-            var priceTd = document.createElement("td");
-            var priceNode = document.createTextNode("$" + product["regularPrice"]);
-            priceTd.appendChild(priceNode);
-            tr.appendChild(priceTd);
-          }
+      tr.appendChild(imgTd);
 
-          // Add shortDescription to row
-          if (product["shortDescription"]) {
-            var descTd = document.createElement("td");
-            var descNode = document.createTextNode(product["shortDescription"]);
-            descTd.appendChild(descNode);
-            tr.appendChild(descTd);
-          }
+      // Add regularPrice to row
+      if (product["regularPrice"]) {
+        var priceTd = document.createElement("td");
+        var priceNode = document.createTextNode("$" + product["regularPrice"]);
+        priceTd.appendChild(priceNode);
+        tr.appendChild(priceTd);
+      }
 
-          table.appendChild(tr);
-        });
+      // Add shortDescription to row
+      if (product["shortDescription"]) {
+        var descTd = document.createElement("td");
+        var descNode = document.createTextNode(product["shortDescription"]);
+        descTd.appendChild(descNode);
+        tr.appendChild(descTd);
+      }
+
+      // Add row to table
+      table.appendChild(tr);
     });
-    document.body.appendChild(table);
   });
+  // Finally, add table to body
+  document.body.appendChild(table);
+});
